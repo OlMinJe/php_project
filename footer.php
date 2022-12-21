8a02446bc8ff33b7eb02d8ebfe74ab65b7dcffdc
OlMinJe <118544134+OlMinJe@users.noreply.github.com>
2022-12-21 19:32:58 +0900

    lmj
    최종본 추가

---
+++ footer.php
@@ -0,0 +1,7 @@
+        <div id="footer_content">
+            <p id="footer_logo">나의 첫 PHP 프로젝트<span>(이민제)</span></p>
+            <ul id="author">
+                <li>문의 메일</li>
+                <li>- 메일 주소 : OlMinJe@gmail.com</li>
+            </ul>
+        </div>
